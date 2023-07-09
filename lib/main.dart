fe8cee5eb734b151418faf9256010ce43d2ad5be
TowaYamashita <Confirm.Apps.Develop@gmail.com>
2023-07-09 16:58:44 +0900

    追加: 画面表示時にコーチマークを表示するよう実装

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,239 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-void main() {
-  runApp(const MyApp());
-}
+import 'package:flutter/material.dart';
+import 'package:tutorial_coach_mark/tutorial_coach_mark.dart';
+
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
+        colorSchemeSeed: Colors.deepOrange,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  MyHomePageState createState() => MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class MyHomePageState extends State<MyHomePage> {
+  late TutorialCoachMark tutorialCoachMark;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  GlobalKey infoButtonKey = GlobalKey();
+  GlobalKey favoriteButtonKey = GlobalKey();
+
+  Map<String, bool> items = {
+    'ALFA': false,
+    'BRAVO': false,
+    'CHARLIE': false,
+    'DELTA': false,
+    'ECHO': false,
+    'FOXTROT': false,
+    'GOLF': false,
+    'HOTEL': false,
+    'INDIA': false,
+    'JULIETT': false,
+    'KILO': false,
+    'LIMA': false,
+    'MIKE': false,
+  };
+
+  @override
+  void initState() {
+    createTutorial();
+    Future.delayed(Duration.zero, showTutorial);
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('コーチマークサンプル'),
+        actions: [
+          IconButton(
+            key: infoButtonKey,
+            onPressed: () {
+              showLicensePage(context: context);
+            },
+            icon: const Icon(Icons.info),
+          ),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+      body: CustomScrollView(
+        slivers: [
+          SliverList.builder(
+            itemBuilder: (_, index) {
+              return ListTile(
+                title: Text(items.keys.elementAt(index)),
+                subtitle: Text('this is test message. ' * 5),
+                trailing: IconButton(
+                  key: index == 0 ? favoriteButtonKey : null,
+                  onPressed: () {
+                    setState(() {
+                      items.update(
+                          items.keys.elementAt(index), (value) => !value);
+                    });
+                  },
+                  icon: Icon(
+                    items.values.elementAt(index)
+                        ? Icons.favorite
+                        : Icons.favorite_outline,
+                    color: Theme.of(context).primaryColor,
+                  ),
+                ),
+                onTap: () {
+                  showModalBottomSheet(
+                    context: context,
+                    showDragHandle: true,
+                    builder: (_) {
+                      return Center(
+                        child: Text('username: ${items[index]}'),
+                      );
+                    },
+                  );
+                },
+              );
+            },
+            itemCount: items.length,
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  void showTutorial() {
+    tutorialCoachMark.show(context: context);
+  }
+
+  void createTutorial() {
+    tutorialCoachMark = TutorialCoachMark(
+      targets: _createTargets(),
+      colorShadow: Colors.deepOrange,
+      alignSkip: Alignment.bottomLeft,
+      skipWidget: ElevatedButton.icon(
+        icon: const Icon(Icons.double_arrow_rounded),
+        label: const Text('スキップ'),
+        onPressed: () {},
+      ),
+      paddingFocus: 5,
+      opacityShadow: 0.8,
+    );
+  }
+
+  List<TargetFocus> _createTargets() {
+    List<TargetFocus> targets = [];
+
+    targets.add(
+      TargetFocus(
+        keyTarget: infoButtonKey,
+        contents: [
+          TargetContent(
+            align: ContentAlign.custom,
+            customPosition: CustomTargetContentPosition(
+              bottom: 0,
+              left: 0,
+            ),
+            builder: (_, controller) {
+              return Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  ElevatedButton.icon(
+                    onPressed: controller.next,
+                    icon: const Icon(Icons.arrow_forward),
+                    label: const Text('次へ'),
+                  ),
+                ],
+              );
+            },
+          ),
+          TargetContent(
+            align: ContentAlign.bottom,
+            builder: (_, controller) {
+              return const Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "ライセンス情報",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      fontSize: 20.0,
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    "このアプリケーションに関するライセンス情報を閲覧したい場合は、このアイコンをタップしてください。",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ],
+              );
+            },
+          ),
+        ],
+      ),
+    );
+    targets.add(
+      TargetFocus(
+        keyTarget: favoriteButtonKey,
+        contents: [
+          TargetContent(
+            align: ContentAlign.custom,
+            customPosition: CustomTargetContentPosition(
+              bottom: 0,
+              left: 0,
+            ),
+            builder: (_, controller) {
+              return Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  ElevatedButton.icon(
+                    onPressed: controller.next,
+                    icon: const Icon(Icons.arrow_forward),
+                    label: const Text('次へ'),
+                  ),
+                ],
+              );
+            },
+          ),
+          TargetContent(
+            align: ContentAlign.bottom,
+            builder: (_, controller) {
+              return const Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "お気に入り",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      fontSize: 20.0,
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    "お気に入りにしたいユーザがいる場合は、そのユーザの横のハートボタンをタップしてください。",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ],
+              );
+            },
+          )
+        ],
+      ),
+    );
+
+    return targets;
   }
 }
